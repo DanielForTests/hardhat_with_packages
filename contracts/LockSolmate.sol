647f9435774bf2d9f32d5f4a3032f32ac5cc4d6e
Daniel PC <raresgeo9@gmail.com>
2024-02-12 14:48:14 +0200

    feat: added solmate

--- contracts/LockSolmate.sol
+++ contracts/LockSolmate.sol
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
 
-contract Lock {
+contract LockSolmate is ERC20 {
     uint public unlockTime;
     address payable public owner;
 
     event Withdrawal(uint amount, uint when);
 
-    constructor(uint _unlockTime) payable {
+    constructor(uint _unlockTime) ERC20("MYTOKEN", "MT", 18) {
         require(
             block.timestamp < _unlockTime,
             "Unlock time should be in the future"
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,28 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
 };
+
+task("remappings", "Outputs the remappings for the project").setAction(
+  async (_, hre) => {
+    const paths = hre.config.paths;
+    const fs = require("fs");
+    const path = require("path");
+
+    const getDirectories = (srcPath) => {
+      return fs
+        .readdirSync(srcPath)
+        .filter((file) => fs.statSync(path.join(srcPath, file)).isDirectory());
+    };
+
+    console.log(paths);
+
+    const remappings = getDirectories(paths.sources).map((dir) => {
+      const libPath = path.join(paths.sources, dir);
+      return `${dir}=${libPath}`;
+    });
+
+    console.log(remappings.join("\n"));
+  }
+);
